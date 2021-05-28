379449089607b5601ba341c0f77a1403379993c8
Hannah Gardner <hannahgardner@Hannahs-MacBook-Pro.local>
2021-05-28 14:08:15 -0500

    binding to heroku's env port

--- server.ts
+++ server.ts
@@ -5,10 +5,11 @@ import * as express from 'express' // JS: const router = express.Router()
 import * as mongoose from 'mongoose' // JS: const mongoose = require("mongoose")
 import * as methodOverride from 'method-override' // JS: const methodOverride = require('method-override')
 import {router} from "./controllers/wellness" // JS: const router = express.Router(), router is in {} to specify which export it is accessing from the controller "wellness"
+import * as session from "express-session"
 
 // ====== CONFIGURATION ===== //
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // ========== //
 app.use(methodOverride('_method'))
@@ -17,6 +18,13 @@ app.use(methodOverride('_method'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
+app.use(
+    session({
+        secret: process.env.SECRET, 
+        resave: false,
+        saveUninitialized: false,
+    })
+)
 
 // ===== CONNECT TO MONGO/EXTERNAL MIDDLEWARE ===== //
 const mongoURI = process.env.LOCAL ? 'mongodb://localhost:27017/basiccrud' : process.env.MONGODBNAME
