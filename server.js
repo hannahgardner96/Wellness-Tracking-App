67f9236f1c4e93192756eb5096359e9f6a89898b
Hannah Gardner <hannahgardner@Hannahs-MacBook-Pro.local>
2021-05-28 14:12:22 -0500

    forgot to compile ts

--- server.js
+++ server.js
@@ -7,15 +7,21 @@ var express = require("express"); // JS: const router = express.Router()
 var mongoose = require("mongoose"); // JS: const mongoose = require("mongoose")
 var methodOverride = require("method-override"); // JS: const methodOverride = require('method-override')
 var wellness_1 = require("./controllers/wellness"); // JS: const router = express.Router(), router is in {} to specify which export it is accessing from the controller "wellness"
+var session = require("express-session");
 // ====== CONFIGURATION ===== //
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 // ========== //
 app.use(methodOverride('_method'));
 // ===== MIDDLEWARE ===== //
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
 // ===== CONNECT TO MONGO/EXTERNAL MIDDLEWARE ===== //
 var mongoURI = process.env.LOCAL ? 'mongodb://localhost:27017/basiccrud' : process.env.MONGODBNAME;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
